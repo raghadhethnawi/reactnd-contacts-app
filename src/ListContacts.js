4a8f3535d6df444f9a01285c666ae68cb45bb15f
raghadhethnawi <raghadhethnawi@gmail.com>
2022-02-20 22:25:18 +0200

    Functional comonent

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,11 +1,9 @@
 import React, {Component} from 'react';
 
-class ListContacts extends Component{
-    render(){
-        // console.log('Props ', this.props)
-        return(
-            <ol className='contact-list'>
-                {this.props.contacts.map((contact)=>(
+function ListContacts(props){
+    return(
+        <ol className='contact-list'>
+                {props.contacts.map((contact)=>(
                     <li key={contact.id} className='contact-list-item'>
                         <div 
                         className='contact-avatar'
@@ -21,8 +19,32 @@ class ListContacts extends Component{
                     </li>
                 ))}
             </ol>
-        )
-    }
+    )
 }
+
+// class ListContacts extends Component{
+//     render(){
+//         // console.log('Props ', this.props)
+//         return(
+//             <ol className='contact-list'>
+//                 {this.props.contacts.map((contact)=>(
+//                     <li key={contact.id} className='contact-list-item'>
+//                         <div 
+//                         className='contact-avatar'
+//                         style={{
+//                             backgroundImage: `url(${contact.avatarURL})`
+//                         }}
+//                         ></div>
+//                         <div className='contact-details'>
+//                             <p>{contact.name}</p>
+//                             <p>{contact.handle}</p>
+//                         </div>
+//                         <button className='contact-remove'>Remove</button>
+//                     </li>
+//                 ))}
+//             </ol>
+//         )
+//     }
+// }
 
 export default ListContacts
